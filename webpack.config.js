21941bf73fa0adde9fae3231cc13cccc590cf796
je-cg <jecg03@hotmail.com>
2016-06-28 11:20:02 -0700

    added boilerplate code

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var config = {
+   entry: './app/main.js',
+	
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
+	
+   devServer: {
+      contentBase: "./public",
+      colors: true,
+      inline: true,
+      port: 8080
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
